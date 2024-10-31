a89b277b70e9374b5424e816542d10e1a29444a0
nzetala <zetalatakamnelson@yahoo.fr>
2024-10-31 12:02:54 +0100

    fix

--- lib/question/translate_word.g.dart
+++ lib/question/translate_word.g.dart
@@ -9,6 +9,9 @@ part of 'translate_word.dart';
 TranslateWord _$TranslateWordFromJson(Map<String, dynamic> json) =>
     TranslateWord(
       questionId: json['questionId'] as String,
+      questionType:
+          $enumDecodeNullable(_$QuestionTypeEnumMap, json['questionType']) ??
+              QuestionType.translateWord,
       wordToTranslate: NativeLanguageText.fromJson(
           json['wordToTranslate'] as Map<String, dynamic>),
       options: (json['options'] as List<dynamic>)
@@ -21,8 +24,24 @@ TranslateWord _$TranslateWordFromJson(Map<String, dynamic> json) =>
 Map<String, dynamic> _$TranslateWordToJson(TranslateWord instance) =>
     <String, dynamic>{
       'questionId': instance.questionId,
+      'questionType': _$QuestionTypeEnumMap[instance.questionType]!,
       'wordToTranslate':
           TranslateWord._wordToTranslateToJson(instance.wordToTranslate),
       'options': TranslateWord._optionsToJson(instance.options),
       'answer': TranslateWord._answerToJson(instance.answer),
     };
+
+const _$QuestionTypeEnumMap = {
+  QuestionType.listenAndSelect: 'listenAndSelect',
+  QuestionType.translateWord: 'translateWord',
+  QuestionType.translateWordReverse: 'translateWordReverse',
+  QuestionType.translateSentence: 'translateSentence',
+  QuestionType.translateSentenceReverse: 'translateSentenceReverse',
+  QuestionType.listenAndIdentify: 'listenAndIdentify',
+  QuestionType.fillInTheBlanks: 'fillInTheBlanks',
+  QuestionType.matchAudioToImage: 'matchAudioToImage',
+  QuestionType.matchPairs: 'matchPairs',
+  QuestionType.translateFromImage: 'translateFromImage',
+  QuestionType.userTranslateSentence: 'userTranslateSentence',
+  QuestionType.writeFromAudio: 'writeFromAudio',
+};
