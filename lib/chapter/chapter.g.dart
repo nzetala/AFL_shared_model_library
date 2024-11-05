7b5676fd7c2291f24bcb8bf307c0f033337fada1
nzetala <zetalatakamnelson@yahoo.fr>
2024-11-05 18:01:42 +0100

    fix chapter

---
+++ lib/chapter/chapter.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chapter.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Chapter _$ChapterFromJson(Map<String, dynamic> json) => Chapter(
+      chapterNumber: json['chapterNumber'] as String,
+    );
+
+Map<String, dynamic> _$ChapterToJson(Chapter instance) => <String, dynamic>{
+      'chapterNumber': instance.chapterNumber,
+    };
---
+++ lib/dictionary/dictionary.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dictionary.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Dictionary _$DictionaryFromJson(Map<String, dynamic> json) => Dictionary(
+      json['name'] as String,
+    );
+
+Map<String, dynamic> _$DictionaryToJson(Dictionary instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
