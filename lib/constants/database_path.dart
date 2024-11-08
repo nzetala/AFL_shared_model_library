e5d67f62d8024ef9fa3017c9fb2ea506cfd7eef7
nzetala <41510734+nzetala@users.noreply.github.com>
2024-11-08 06:29:33 +0100

    add path

--- lib/constants/database_path.dart
+++ lib/constants/database_path.dart
@@ -1,3 +1,9 @@
 class DatabasePath {
-  static const String courses = '/DEV/Learn/Courses';
+  static String getCoursesPath() => '/DEV/Learn/Courses';
+
+  static String getLexiconPath(String courseId) =>
+      '/DEV/Learn/Courses/$courseId/lexicon';
+
+  static String getWordPath(String courseId, String wordId) =>
+      '/DEV/Learn/Courses/$courseId/lexicon/$wordId';
 }
